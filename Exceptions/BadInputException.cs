6f9b028b6c04a268aa35fd986ad0490c209e7dbd
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-04-15 13:08:51 +0800

    Added Exceptions

---
+++ Exceptions/BadInputException.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomerAPI.Dtos
+{
+    /// <summary>
+    /// The bad input exception.
+    /// </summary>
+    /// <seealso cref="BaseException" />
+    public class BadInputException : BaseException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BadInputException"/> class.
+        /// </summary>
+        /// <param name="errorInfo">The error info.</param>
+        public BadInputException(ErrorInfo errorInfo)
+            : base(errorInfo)
+        {
+        }
+    }
+}
