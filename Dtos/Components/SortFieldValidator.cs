133a17567197c38b85eb0a2f23c1873b3c577fe4
Dondon Herrera <dondon.herrera@kobkiat-it.com>
2021-04-15 13:09:13 +0800

    Added Components under DTOs

---
+++ Dtos/Components/SortFieldValidator.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+
+namespace CustomerAPI.Dtos
+{
+    /// <summary>
+    /// The sort field validator.
+    /// </summary>
+    public static class SortFieldValidator
+    {
+        /// <summary>
+        /// Validates sorting field is belong with T or not.
+        /// </summary>
+        /// <typeparam name="T">The database model.</typeparam>
+        /// <param name="sortingField">The sorting field.</param>
+        /// <param name="validSortingField">The valid sorting field.</param>
+        /// <returns>The error info object.</returns>
+        public static ErrorInfo Validate<T>(string sortingField, out string validSortingField) where T : class
+        {
+            sortingField = string.IsNullOrWhiteSpace(sortingField) ? "FullName" : sortingField;
+
+            validSortingField = null;
+
+            var e = new ErrorInfo();
+
+            var propertyInfo = typeof(T).GetProperty(sortingField, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+            if (propertyInfo is null)
+            {
+                e.ErrorCode = ErrorTypes.InvalidSortField;
+                e.ErrorMessage = $"Sort field '{sortingField}' is invalid.";
+                return e;
+            }
+
+            validSortingField = propertyInfo.Name;
+
+            return e;
+        }
+    }
+}
---
+++ Dtos/ProblemDetailsDto.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace CustomerAPI.Dtos
+{
+    /// <summary>
+    /// The problem details dto.
+    /// </summary>
+    public class ProblemDetailsDto
+    {
+        /// <summary>
+        /// Gets or sets the error code.
+        /// </summary>
+        /// <value>
+        /// The error code.
+        /// </value>
+        public int? ErrorCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the detail.
+        /// </summary>
+        /// <value>
+        /// The detail.
+        /// </value>
+        public string Detail { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the instance.
+        /// </summary>
+        /// <value>
+        /// The instance.
+        /// </value>
+        public string Instance { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status.
+        /// </summary>
+        /// <value>
+        /// The status.
+        /// </value>
+        public int? Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the extensions.
+        /// </summary>
+        /// <value>
+        /// The extensions.
+        /// </value>
+        public IDictionary<string, object> Extensions { get; set; }
+    }
+}
